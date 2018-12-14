25f41ec17d78e9f5518e972789697c0015f333e2
Wendell Misiedjan <wendellmisiedjan@gmail.com>
2018-12-14 18:29:20 +0100

    refactor: remove unused component from atoms

--- src/atoms/highlight/elements.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/macro';
-
-/**
- * The highlight container groups the content using a paragraph.
- * It is used to mark the content as readable text only.
- */
-export const HighlightContainer = styled.p`
-	margin: 0;
-`;
